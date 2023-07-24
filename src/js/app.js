43959fecddf1edd8344bd40384b139191750cb56
Andrey Burlakov <andrew.burlackov@yandex.ru>
2023-07-24 13:34:12 +0300

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+import './characters/character';
+import './characters/bowerman';
+import './characters/swordsman';
+import './characters/magician';
+import './characters/daemon';
+import './characters/undead';
+import './characters/zombie';
